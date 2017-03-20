dedd56a3727be6166de7330bee471892241b87c9
Adrian John Aspiras <aj_aspiras@yahoo.com>
2017-03-20 23:48:53 +0800

    Add files via upload

---
+++ WebTekLEc/script/script.js
@@ -0,0 +1,71 @@
+var x = document.getElementById("topic1items");
+var y = document.getElementById("topic2items");
+var z = document.getElementById("topic3items");
+var a = document.getElementById("topic4items");
+var b = document.getElementById("topic5items");
+var c = document.getElementById("topic6items");
+var d = document.getElementById("topic0items");
+x.style.display = 'none';
+y.style.display = 'none';
+z.style.display = 'none';
+a.style.display = 'none';
+b.style.display = 'none';
+c.style.display = 'none';
+d.style.display = 'none';
+
+function toggleTopic1() {
+    		var x = document.getElementById("topic1items");
+			    if (x.style.display === 'none') {
+			        x.style.display = 'block';
+			    } else {
+			        x.style.display = 'none';
+			    }
+		}
+		function toggleTopic2() {
+    		var x = document.getElementById("topic2items");
+			    if (x.style.display === 'none') {
+			        x.style.display = 'block';
+			    } else {
+			        x.style.display = 'none';
+			    }
+		}
+		function toggleTopic3() {
+    		var x = document.getElementById("topic3items");
+			    if (x.style.display === 'none') {
+			        x.style.display = 'block';
+			    } else {
+			        x.style.display = 'none';
+			    }
+		}
+		function toggleTopic4() {
+    		var x = document.getElementById("topic4items");
+			    if (x.style.display === 'none') {
+			        x.style.display = 'block';
+			    } else {
+			        x.style.display = 'none';
+			    }
+		}
+		function toggleTopic5() {
+    		var x = document.getElementById("topic5items");
+			    if (x.style.display === 'none') {
+			        x.style.display = 'block';
+			    } else {
+			        x.style.display = 'none';
+			    }
+		}
+		function toggleTopic6() {
+    		var x = document.getElementById("topic6items");
+			    if (x.style.display === 'none') {
+			        x.style.display = 'block';
+			    } else {
+			        x.style.display = 'none';
+			    }
+		}
+		function toggleTopic0() {
+    		var x = document.getElementById("topic0items");
+			    if (x.style.display === 'none') {
+			        x.style.display = 'block';
+			    } else {
+			        x.style.display = 'none';
+			    }
+		}
