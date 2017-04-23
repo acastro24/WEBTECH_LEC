3285d361349008c25035c6dc27488ecd4f05c6b8
Adrian John Aspiras <aj_aspiras@yahoo.com>
2017-04-23 16:24:48 +0800

    Added most client side scripting topics

--- WebTekLEc/script/midscript.js
+++ WebTekLEc/script/midscript.js
@@ -5,12 +5,20 @@ var z = document.getElementById("midtopic3items");
 var a = document.getElementById("midtopic4items");
 var b = document.getElementById("midtopic5items");
 var c = document.getElementById("midtopic6items");
+var e = document.getElementById("midtopic7items");
+var f = document.getElementById("midtopic8items");
+var g = document.getElementById("midtopic9items");
+var h = document.getElementById("midtopic10items");
+h.style.display = 'none';
+g.style.display = 'none';
+f.style.display = 'none';
+e.style.display = 'none';
 x.style.display = 'none';
 y.style.display = 'none';
 z.style.display = 'none';
 a.style.display = 'none';
 b.style.display = 'none';
-//c.style.display = 'none';
+c.style.display = 'none';
 d.style.display = 'none';
 
 function toggleTopic(indx) {
